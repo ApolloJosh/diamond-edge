27af2fc0a37940cc8f28b77d4d71686370a97e8d
ApolloJosh <joshstros1@gmail.com>
2026-04-29 16:20:46 -0500

    v23

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Diamond Edge Service Worker — cache-first for assets, network-first for API
-var CACHE_NAME = 'diamond-edge-v21';
+var CACHE_NAME = 'diamond-edge-v23';
 var STATIC_ASSETS = [
   './',
   './index.html',
